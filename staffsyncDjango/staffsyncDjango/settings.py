21019df14a2f04f3482e8a2cf49782257ad520d5
itskhushee <khusheevakil@gmail.com>
2024-11-17 14:23:09 -0500

    Add .gitignore and update settings.py

--- staffsyncDjango/staffsyncDjango/settings.py
+++ staffsyncDjango/staffsyncDjango/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'staffsyncApp'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,12 @@ WSGI_APPLICATION = 'staffsyncDjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'StaffSync',
+        'USER': 'root',
+        'PASSWORD': 'khuloveru0716',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
