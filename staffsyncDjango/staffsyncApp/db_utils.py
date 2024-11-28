417907570f682021756cc8f7a042f543d436e4f1
shashwattiwari <rushilshashwat@gmail.com>
2024-11-28 14:34:04 -0500

    call_procedure updated

--- staffsyncDjango/staffsyncApp/db_utils.py
+++ staffsyncDjango/staffsyncApp/db_utils.py
@@ -13,17 +13,43 @@ def execute_query(query, params=None):
             return cursor.rowcount
 
 
+# def call_procedure(procedure_name, params=None):
+#     with connection.cursor() as cursor:
+#         if params:
+#             cursor.callproc(procedure_name, params)
+#         else:
+#             cursor.callproc(procedure_name)
+#
+#         if cursor.description:
+#             columns = [col[0] for col in cursor.description]
+#             return [dict(zip(columns, row)) for row in cursor.fetchall()]
+#         return cursor.rowcount
+#
 
 def call_procedure(procedure_name, params=None):
     with connection.cursor() as cursor:
         if params:
-            cursor.callproc(procedure_name, params)
+            out_params = [p for p in params if isinstance(p, OutParam)]
+            in_params = [p for p in params if not isinstance(p, OutParam)]
+            cursor.callproc(procedure_name, in_params + [p.value for p in out_params])
+
+            # Fetch any result sets
+            results = list(cursor.fetchall())
+
+            # Get OUT parameter values
+            if out_params:
+                cursor.execute('SELECT ' + ','.join(['@_' + procedure_name + '_' + str(i) for i in
+                                                     range(len(in_params), len(params))]))
+                out_values = cursor.fetchone()
+                for i, out_param in enumerate(out_params):
+                    out_param.value = out_values[i]
         else:
             cursor.callproc(procedure_name)
+            results = list(cursor.fetchall())
 
         if cursor.description:
             columns = [col[0] for col in cursor.description]
-            return [dict(zip(columns, row)) for row in cursor.fetchall()]
+            return [dict(zip(columns, row)) for row in results]
         return cursor.rowcount
 
 
